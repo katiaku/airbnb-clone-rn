9beaeb50bb61c18058630e4e1ed55917a0f5aae2
Ekaterina Kushnir <kushnir.ekaterina@gmail.com>
2024-12-29 17:59:56 +0100

    add clerk auth with apple, google & facebook

--- app/(modals)/login.tsx
+++ app/(modals)/login.tsx
@@ -1,11 +1,133 @@
-import { View, Text } from 'react-native'
+import Colors from '@/constants/Colors';
+import { defaultStyles } from '@/constants/Styles';
+import { useWarmUpBrowser } from '@/hooks/useWarmUpBrowser'
+import { useOAuth } from '@clerk/clerk-expo';
+import { Ionicons } from '@expo/vector-icons';
+import { useRouter } from 'expo-router';
+import { View, StyleSheet, TextInput, TouchableOpacity, Text } from 'react-native';
+
+enum Strategy {
+  Google = 'oauth_google',
+  Apple = 'oauth_apple',
+  Facebook = 'oauth_facebook',
+}
 
 const Page = () => {
+  useWarmUpBrowser();
+
+  const router = useRouter();
+
+  const { startOAuthFlow: googleAuth } = useOAuth({ strategy: 'oauth_google' });
+  const { startOAuthFlow: appleAuth } = useOAuth({ strategy: 'oauth_apple' });
+  const { startOAuthFlow: facebookAuth } = useOAuth({ strategy: 'oauth_facebook' });
+
+  const onSelectAuth = async (strategy: Strategy) => {
+    const selectedAuth = {
+      [Strategy.Google]: googleAuth,
+      [Strategy.Apple]: appleAuth,
+      [Strategy.Facebook]: facebookAuth,
+    }[strategy];
+
+    try {
+      const { createdSessionId, setActive } = await selectedAuth();
+      console.log(createdSessionId, setActive);
+
+      if (createdSessionId) {
+        setActive!({ session: createdSessionId});
+        router.back();
+      }
+    } catch (err) {
+      console.error("OAuth error: ", err);
+    }
+  }
+
   return (
-    <View>
-      <Text>Login</Text>
+    <View style={styles.container}>
+      <TextInput 
+        autoCapitalize="none" 
+        placeholder="Email" 
+        style={[defaultStyles.inputField, { marginBottom: 30}]} 
+      />
+      <TouchableOpacity style={defaultStyles.btn}>
+        <Text style={defaultStyles.btnText}>Continue</Text>
+      </TouchableOpacity>
+      <View style={styles.separatorView}>
+        <View 
+          style={{
+            flex: 1,
+            borderBottomColor: "#000",
+            borderBottomWidth: StyleSheet.hairlineWidth,
+          }}
+        />
+        <Text style={styles.separator}>or</Text>
+        <View 
+          style={{
+            flex: 1,
+            borderBottomColor: "#000",
+            borderBottomWidth: StyleSheet.hairlineWidth,
+          }}
+        />
+      </View>
+
+      <View style={{ gap: 20 }}>
+        <TouchableOpacity style={styles.btnOutline}>
+          <Ionicons name="call-outline" size={24} style={defaultStyles.btnIcon} />
+          <Text style={styles.btnOutlineText}>Continue with Phone</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.btnOutline} onPress={() => onSelectAuth(Strategy.Apple)}>
+          <Ionicons name="logo-apple" size={24} style={defaultStyles.btnIcon} />
+          <Text style={styles.btnOutlineText}>Continue with Apple</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.btnOutline} onPress={() => onSelectAuth(Strategy.Google)}>
+          <Ionicons name="logo-google" size={24} style={defaultStyles.btnIcon} color={'#3BAB58'} />
+          <Text style={styles.btnOutlineText}>Continue with Google</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.btnOutline} onPress={() => onSelectAuth(Strategy.Facebook)}>
+          <Ionicons name="logo-facebook" size={24} style={defaultStyles.btnIcon} color={'#415CA0'} />
+          <Text style={styles.btnOutlineText}>Continue with Facebook</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    padding: 26,
+  },
+  separatorView: {
+    flexDirection: 'row',
+    gap: 10,
+    alignItems: 'center',
+    marginVertical: 30,
+  },
+  separator: {
+    fontFamily: 'montserrat',
+    fontWeight: '700',
+    color: Colors.grey,
+  },
+  btnOutline: {
+    backgroundColor: '#fff',
+    borderWidth: 1,
+    borderColor: Colors.grey,
+    height: 50,
+    borderRadius: 8,
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'row',
+    paddingHorizontal: 10,
+  },
+  btnOutlineText: {
+    color: '#000',
+    fontSize: 16,
+    fontFamily: 'montserrat',
+    fontWeight: '700',
+  },
+})
 
 export default Page
--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,9 +1,18 @@
-import { View, Text } from 'react-native'
+import { useAuth } from '@clerk/clerk-expo';
+import { Link } from 'expo-router';
+import { View, Button, Text } from 'react-native'
 
 const Page = () => {
+  const { signOut, isSignedIn } = useAuth();
+
   return (
     <View>
-      <Text>Profile</Text>
+      <Button title='Log out' onPress={() => signOut()} />
+      { !isSignedIn && (
+        <Link href={'/(modals)/login'}>
+          <Text>Log in</Text>
+        </Link>
+      )}
     </View>
   )
 }
